a1531e996c13d830fcdd19828561a9fcb1b71f78
Salim Dason <dasonsalim@outlook.com>
2022-01-07 17:58:59 +0100

    updates

--- storefront/views.py
+++ storefront/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 
 def storehome(request):
-    return render(request, 'storefront/index.html')
+    return render(request, 'storefront/home.html')
 
-# from django.template.loader import get_template
-# from django.http import HttpResponse
-
-# def home(request):
-#     template = get_template('app1/home.html')
-#     context = {
-
-#     }
-#     return HttpResponse(template.render(context, request))
