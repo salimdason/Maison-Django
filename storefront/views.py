f4add2fb5a0da26eecbd2a3a064731cc9586b63c
Salim Dason <dasonsalim@outlook.com>
2022-01-08 10:41:03 +0100

    updates

--- storefront/views.py
+++ storefront/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import *
+
 
 def storehome(request):
-    return render(request, 'storefront/home.html')
+    products = Product.objects.all()
+    context= {'products':products}
+    return render(request, 'storefront/home.html', context)
 
+def cart(request):
+    context= {}
+    return render(request, 'storefront/cart.html', context)
+
+def checkout(request):
+    context= {}
+    return render(request, 'storefront/checkout.html', context)
