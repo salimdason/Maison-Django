214fbe71c8b9bd2e601400ed95c7dd7f4ec731c1
Salim Dason <dasonsalim@outlook.com>
2022-01-11 13:01:36 +0100

    urls.py edit to include additional routes

--- storefront/urls.py
+++ storefront/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.storehome, name='store-home')
+    path('', views.storehome, name='store-home'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('products/', views.products, name='products'),
 ]
