24007d3d8fe1d6c561d073fdb9fc6b1cdea711a2
Salim Dason <dasonsalim@outlook.com>
2022-01-11 13:02:56 +0100

    views upates to include context data from querysets

--- storefront/views.py
+++ storefront/views.py
@@ -8,9 +8,40 @@ def storehome(request):
     return render(request, 'storefront/home.html', context)
 
 def cart(request):
-    context= {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_total":0, "get_cart_items":0}
+
+    context= {"items": items, "order":order}
     return render(request, 'storefront/cart.html', context)
 
 def checkout(request):
-    context= {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_cart_total":0, "get_cart_items":0}
+
+    context= {"items": items, "order":order}
+
     return render(request, 'storefront/checkout.html', context)
+
+def products(request):
+    context= {}
+    return render(request, 'storefront/products.html', context)
+
+def about(request):
+    context= {}
+    return render(request, 'storefront/about.html', context)
+
+def contact(request):
+    context= {}
+    return render(request, 'storefront/contact.html', context)
