ea40da762b947bb8da24eade225a6d8aefa2e5ee
Salim Dason <dasonsalim@outlook.com>
2022-01-11 12:41:50 +0100

    corrected issues with OrderItems table

--- MaisonDjango/settings.py
+++ MaisonDjango/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'storefront',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
